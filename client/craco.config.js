966fc8ff3138e2fb5395660ffec7c5939c82ccc2
JoaoFernandesServinform <joaoantonio.rodrigues@servinformgroup.com>
2025-06-07 20:42:09 +0100

    fix: Provide explicit config to craco-workbox plugin

--- client/craco.config.js
+++ client/craco.config.js
@@ -1,12 +1,18 @@
 // client/craco.config.js
-const CracoWorkboxPlugin = require('craco-workbox'); // Corrected package name
+const CracoWorkboxPlugin = require('craco-workbox');
 
 module.exports = {
-  plugins: [
-    {
-      plugin: CracoWorkboxPlugin,
-      // No options are needed for the default behavior,
-      // it will automatically find your custom-service-worker.js
+  webpack: {
+    plugins: {
+      add: [
+        new CracoWorkboxPlugin({
+          // This is the key part: we explicitly tell it where to find the source service worker
+          // and what to name the output file. This overrides the default behavior of
+          // looking for a workbox.config.js file.
+          swSrc: './public/custom-service-worker.js',
+          swDest: 'service-worker.js',
+        }),
+      ],
     },
-  ],
+  },
 };
