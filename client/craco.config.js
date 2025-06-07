2810433f10ee6a1021c39fb89cf295a3ad3beb9e
JoaoFernandesServinform <joaoantonio.rodrigues@servinformgroup.com>
2025-06-07 21:15:07 +0100

    Please

--- client/craco.config.js
+++ client/craco.config.js
@@ -1,31 +1,19 @@
 // client/craco.config.js
-const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+const { InjectManifest } = require('workbox-webpack-plugin');
 const path = require('path');
 
 module.exports = {
   webpack: {
-    configure: (webpackConfig, { env, paths }) => {
-      // Only add the Workbox plugin in production builds
-      if (env === 'production') {
-        // Find the existing GenerateSW plugin added by Create React App
-        const generateSwPlugin = webpackConfig.plugins.find(
-          (plugin) => plugin.constructor.name === 'GenerateSW'
-        );
-
-        // If the default plugin exists, replace it with our custom InjectManifest
-        if (generateSwPlugin) {
-          webpackConfig.plugins.splice(
-            webpackConfig.plugins.indexOf(generateSwPlugin),
-            1,
-            new WorkboxWebpackPlugin.InjectManifest({
-              swSrc: path.resolve(__dirname, 'public', 'custom-service-worker.js'),
-              swDest: 'service-worker.js',
-            })
-          );
-        }
-      }
-
-      return webpackConfig;
+    plugins: {
+      // We are REMOVING the default CRA service worker plugin...
+      remove: ['GenerateSW'],
+      // ...and ADDING our own custom one.
+      add: [
+        new InjectManifest({
+          swSrc: path.join(__dirname, 'public', 'custom-service-worker.js'),
+          swDest: 'service-worker.js',
+        }),
+      ],
     },
   },
 };
